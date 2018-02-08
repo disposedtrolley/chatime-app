5882c8c442bb81ff54bcd5fd7512922589464217
jamesliu <jamesliu@au1.ibm.com>
2018-02-09 10:27:45 +1100

    replace if statement with try/catch

--- tools/data-ingest/json-formatter.js
+++ tools/data-ingest/json-formatter.js
@@ -1,4 +1,5 @@
 const isArray = require('util').isArray
+const isNull = require('util').isNull
 const objectPath = require('object-path')
 
 
@@ -10,16 +11,15 @@ class JsonFormatter {
 		const keys = Object.keys(jsonObj)
 
 		for (let key of keys) {
-
 			// Get the associated output key array.
 			const outputKeyArray = mapping[key]
 
-			if (isArray(outputKeyArray)) {
+			try {
 				// Library to set an object's property value based on
 				// an array representation of its position.
 				objectPath.set(output, outputKeyArray, jsonObj[key])
-			} else {
-				console.error('Key mapping value should be an array.')
+			} catch(e) {
+				console.error(e)
 			}
 		}
 		return output
