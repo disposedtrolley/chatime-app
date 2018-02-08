2580801450f4032f7b685d9f840084f86c470817
jamesliu <jamesliu@au1.ibm.com>
2018-02-09 10:39:48 +1100

    update index.js to work with changes to the formatter

--- tools/data-ingest/index.js
+++ tools/data-ingest/index.js
@@ -1,6 +1,7 @@
 const csvFilePath = '../../data/tea_data_mixins.csv'
 const CsvIngest = require('./csv-ingest')
 const JsonFormatter = require('./json-formatter')
+const mappings = require('../config').DATA_FORMAT.KEY_MAPPING
 
 
 const ingestor = new CsvIngest(csvFilePath)
@@ -10,5 +11,5 @@ ingestor.ingest()
 	.then(results => {
 		console.log(results[0])
 
-		formatter.formatMixin(results[0])
+		formatter.format(results[0], mappings.MIXIN)
 	})
