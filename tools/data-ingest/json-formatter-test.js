262361ae03301507c162aa037df802ae9278a120
jamesliu <jamesliu@au1.ibm.com>
2018-02-09 10:30:48 +1100

    add tests for base level properties, rename test to conform with new function name

--- tools/data-ingest/json-formatter-test.js
+++ tools/data-ingest/json-formatter-test.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert
 const JsonFormatter = require('./json-formatter')
 
 describe('JsonFormatter', function() {
-	describe('formatMixin', function() {
+	describe('format', function() {
 
 		const formatter = new JsonFormatter()
 
@@ -37,11 +37,20 @@ describe('JsonFormatter', function() {
 			assert.equal(Object.keys(output), 0, 'the output is empty')
 		})
 
+		it('should correctly process base level properties', function() {
+			const baseProperty = testMapping['Flavour']
+			const output = formatter.format(testInput, testMapping)
+
+			assert.exists(output[baseProperty], 'the base property exists in the output')
+			assert.equal(output[baseProperty], testInput['Flavour'], 'the base property value is correct')
+		})
+
 		it('should correctly process nested properties', function() {
 			const nestedProperties = testMapping['Kilojules']
 			const output = formatter.format(testInput, testMapping)
 
 			assert.exists(output[nestedProperties[0]][nestedProperties[1]], 'the nested property exists in the output')
+			assert.equal(output[nestedProperties[0]][nestedProperties[1]], testInput['Kilojules'], 'the nested property value is correct')
 		})
 
 		it('should not add a property if its value in the mapping is empty', function() {
