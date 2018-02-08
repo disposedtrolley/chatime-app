115f9881f91279cf0d04f515b84adf8c67c98062
jamesliu <jamesliu@au1.ibm.com>
2018-02-08 11:57:14 +1100

    add dependency to convert from array to object property path, and add sample call to test

---
+++ tools/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	DATA_FORMAT: {
+		KEY_MAPPING: {
+			MIXIN: {
+				'Flavour': ['name'],
+				'Hot drinks': null,
+				'Kilojules': ['nutritionalInformation', 'kilojoules'],
+				'Limited edition': ['specialAttributes', 'isLimitedEdition']
+			}
+		}
+	}
+}
--- tools/data-ingest/index.js
+++ tools/data-ingest/index.js
@@ -1,10 +1,14 @@
-const csvFilePath = '../../data/tea_data_all.csv'
+const csvFilePath = '../../data/tea_data_mixins.csv'
 const CsvIngest = require('./csv-ingest')
+const JsonFormatter = require('./json-formatter')
 
 
 const ingestor = new CsvIngest(csvFilePath)
+const formatter = new JsonFormatter()
 
 ingestor.ingest()
 	.then(results => {
-		console.log(results)
+		console.log(results[0])
+
+		formatter.formatMixin(results[0])
 	})
