7ae2385fddeb47aa0ed715f9d3263189dcce275f
jamesliu <jamesliu@au1.ibm.com>
2018-02-09 10:27:26 +1100

    additional unit tests

--- tools/data-ingest/json-formatter-test.js
+++ tools/data-ingest/json-formatter-test.js
@@ -1,5 +1,4 @@
 const assert = require('chai').assert
-const mappings = require('../config').DATA_FORMAT.KEY_MAPPING
 const JsonFormatter = require('./json-formatter')
 
 describe('JsonFormatter', function() {
@@ -14,12 +13,42 @@ describe('JsonFormatter', function() {
 			'Limited edition': '0'
 		}
 
-		const testMapping = mappings.MIXIN
+		const testMapping = {
+			'Flavour': ['name'],
+			'Hot drinks': null,
+			'Kilojules': ['nutritionalInformation', 'kilojoules'],
+			'Limited edition': ['specialAttributes', 'isLimitedEdition']
+		}
 
 		it('should return an object', function() {
 			const output = formatter.format(testInput, testMapping)
 
 			assert.typeOf(output, 'object', 'the output is an object')
+		})
+
+		it('should return an empty object if an incorrect mapping is found', function() {
+			const badMapping = {
+				'a': 'b'
+			}
+
+			const output = formatter.format(testInput, badMapping)
+		
+			assert.typeOf(output, 'object', 'the output is an object')
+			assert.equal(Object.keys(output), 0, 'the output is empty')
+		})
+
+		it('should correctly process nested properties', function() {
+			const nestedProperties = testMapping['Kilojules']
+			const output = formatter.format(testInput, testMapping)
+
+			assert.exists(output[nestedProperties[0]][nestedProperties[1]], 'the nested property exists in the output')
+		})
+
+		it('should not add a property if its value in the mapping is empty', function() {
+			const emptyProperty = testMapping['Hot drinks']
+			const output = formatter.format(testInput, testMapping)
+
+			assert.notExists(output[emptyProperty], 'empty properties in the mapping are not added to the output')
 		})
 	})
 	
