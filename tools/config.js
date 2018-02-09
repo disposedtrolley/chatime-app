a5b4685152c0624871261e2d6a10f581c9f60b90
jamesliu <jamesliu@au1.ibm.com>
2018-02-09 11:56:55 +1100

    add mappings for teas and restrictions

--- tools/config.js
+++ tools/config.js
@@ -6,6 +6,23 @@ module.exports = {
 				'Hot drinks': null,
 				'Kilojules': ['nutritionalInformation', 'kilojoules'],
 				'Limited edition': ['specialAttributes', 'isLimitedEdition']
+			},
+			PRODUCT: {
+				'Category': ['type'],
+				'Tea type': ['subtype'],
+				'Flavour': ['flavour'],
+				'Size': ['servingSize', 'storeSize'],
+				'Kilojules': ['nutritionalInformation']['kilojoules'],
+				'Contains sugar': null,
+				'Limited edition': ['specialAttributes', 'isLimitedEdition']
+			},
+			RESTRICTION: {
+				'Category': ['type'],
+				'Can change tea type': ['canModifySubtype'],
+				'Can change size': ['canModifySize'],
+				'Can add mix-ins': ['canAddCustomMixins'],
+				'Can change ice': ['canModifyBaseMixins', 'ice'],
+				'Can change sugar': ['canModifyBaseMixins', 'sugar']
 			}
 		}
 	}
