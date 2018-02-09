60fcbb16b6ed223a02bd3c0a4e0e3bba17375e6c
jamesliu <jamesliu@au1.ibm.com>
2018-02-09 11:13:48 +1100

    update index to export CsvIngest and JsonFormatter

--- tools/data-ingest/index.js
+++ tools/data-ingest/index.js
@@ -1,15 +1,4 @@
-const csvFilePath = '../../data/tea_data_mixins.csv'
-const CsvIngest = require('./csv-ingest')
-const JsonFormatter = require('./json-formatter')
-const mappings = require('../config').DATA_FORMAT.KEY_MAPPING
-
-
-const ingestor = new CsvIngest(csvFilePath)
-const formatter = new JsonFormatter()
-
-ingestor.ingest()
-	.then(results => {
-		console.log(results[0])
-
-		formatter.format(results[0], mappings.MIXIN)
-	})
+module.exports = {
+	CsvIngest: require('./csv-ingest'),
+	JsonFormatter: require('./json-formatter')
+}
